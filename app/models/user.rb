4d9c391acf8cdd4fa72502567631e27b8ddbb8f5
obibaadoma <akrasikonadu@yahoo.com>
2023-11-03 16:22:48 +0000

    reviewed changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: :author_id
   has_many :posts, foreign_key: :author_id
 
-  validates :post_counter, numericality: { only_integer: true }
+  validates :postsCounter, numericality: { only_integer: true }
   validates :bio, presence: true
   validates :photo, presence: true
   validates :name, presence: true
