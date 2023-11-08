d549135d124d47b16465562bd635015c32d7b043
obibaadoma <akrasikonadu@yahoo.com>
2023-11-08 23:43:52 +0000

    fixed linters and reviewer suggestions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
+  layout 'standard'
+  def index
+    @users = User.order(id: :asc)
+  end
+
   def show
     @user = User.find(params[:id])
-    render 'users/show'
   end
 end
