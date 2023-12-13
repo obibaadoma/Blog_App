87e78ff0df9b56b587e9cf6b65650b77550b4e64
obibaadoma <akrasikonadu@yahoo.com>
2023-12-13 23:36:59 +0000

    added devise

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,10 +2,12 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:id])
     @userposts = @user.posts.includes(:comments)
+    render json: @posts, status: :ok
   end
 
   def show
     @post = Post.find(params[:id])
+    render json: @posts, status: :ok
     @current_user = current_user
   end
 
@@ -32,5 +34,11 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:title, :text)
+  end
+
+  def destroy
+    authorize! :destroy, @post
+    @post.destroy
+    redirect_to posts_path, notice: 'Post was successfully deleted.'
   end
 end
